10ff7f8805d6d01c403aa2ecf5639ca245b36d40
Bill Zerega <zerega85@gmail.com>
2016-02-25 15:36:57 -0500

    got account details working

--- main.rb
+++ main.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-
+require 'sinatra/flash'
+enable :sessions
 #set the database
 configure(:development){set :database, 'sqlite3:microblog.sqlite3'}
 
+set :sessions, true
 require './models/users'
 require './models/posts'
 
@@ -21,6 +23,7 @@ get '/about' do
 end
 
 get '/profile' do
+  current_user
   erb :profileview
 end
 
@@ -33,6 +36,21 @@ post '/submitted' do
    @user_date = params[:joined]
    @user_password = params[:password]
    @user = User.create(fname: @user_fname, lname: @user_lname, email: @user_email, username: @user_name, description: @user_description, joined: @user_date, password: @user_password )
-
+   redirect '/'
 end
 
+post '/sign-in' do
+  @user = User.where(email: params[:email]).first
+  if @user && @user.password === params[:password]
+    session[:user_id] = @user.id
+    @logged_in = true
+    flash[:notice] = "terrific sign in"
+    redirect '/'
+   end
+end
+
+def current_user
+  if session[:user_id]
+   @current_user = User.find(session[:user_id])
+  end
+end
