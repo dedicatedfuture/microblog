f82f2ddec1eebfed3dd1e372b93d62dedce6e754
sharpattack <sharp1209@gmail.com>
2016-02-27 13:40:33 -0500

    trying to grab bill's changes

--- models/users.rb
+++ models/users.rb
@@ -1,16 +1,3 @@
 class User < ActiveRecord::Base
   has_many :posts
-
-    def get_followers
-
-        followersArray = []
-        follower_ids = Follower.where(following: self.id).pluck(:followed)
-
-        follower_ids.each do |id|
-          followersArray.push User.find id
-        end
-
-    return followersArray
-
-    end
 end
