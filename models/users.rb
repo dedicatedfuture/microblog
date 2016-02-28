a0db30f2ff6617da7375953ad98b927f74dda6eb
Bill Zerega <zerega85@gmail.com>
2016-02-28 18:40:56 -0500

    up to date for austin

--- models/users.rb
+++ models/users.rb
@@ -2,16 +2,14 @@ class User < ActiveRecord::Base
   has_many :posts
 
   def get_followers
+
+
     followersArray = []
     follower_ids = Follower.where(following: self.id).pluck(:followed)
 
-
-        followersArray = []
-        follower_ids = Follower.where(following: self.id).pluck(:followed)
-
-        follower_ids.each do |id|
-          followersArray.push(User.where(id))
-        end
+    follower_ids.each do |id|
+       followersArray.push(User.where(id).username)
+    end
 
 
 
