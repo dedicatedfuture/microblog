3b8eb3fecc159ff242eeaf4d29a0a08f63b2c5da
sharpattack <sharp1209@gmail.com>
2016-02-28 20:27:57 -0500

    got the followers/following list to work

--- models/users.rb
+++ models/users.rb
@@ -3,18 +3,27 @@ class User < ActiveRecord::Base
 
   def get_followers
 
-
     followersArray = []
     follower_ids = Follower.where(following: self.id).pluck(:followed)
 
-    follower_ids.each do |id|
-       followersArray.push(User.where(id).username)
+    follower_ids.each do |i|
+       followersArray.push(User.where(id: i).first.username)
     end
-
-
-
 
     return followersArray
 
   end
+
+  def get_following
+
+    followingArray = []
+    following_ids = Follower.where(followed: self.id).pluck(:following)
+
+    following_ids.each do |i|
+      followingArray.push(User.where(id: i).first.username)
+    end
+
+    return followingArray
+  end
+
 end
