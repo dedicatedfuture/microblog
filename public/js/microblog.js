c81aef5d129f8b2a3807c90728b6d7af45d5e1cd
sharpattack <sharp1209@gmail.com>
2016-02-29 19:23:49 -0500

    some styling, tried swapping the login/logout with javsascript

--- public/js/microblog.js
+++ public/js/microblog.js
@@ -1,7 +1,20 @@
+// $(document).ready(function(){
+//   $("#logout").click(function(){
+//     $("#logout").hide();
+//     $("signin").show();
+//     $("signup").show();
+//   });
+//
+//   $("#signin").click(function(){
+//     $("#signin").hide();
+//     $("#signup").hide();
+//     $("#logout").show();
+//   });
+// });
+
 function confirmDelete() {
 if (window.confirm("Are you sure you want to delete your profile? There is no retreival")){
     window.open("http://localhost:9393/del")
 }
 
 }
-
