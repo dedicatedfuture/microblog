51ead70c0e4cb7c9663efbc2892cacb5e1555872
sharpattack <sharp1209@gmail.com>
2016-03-01 09:53:22 -0500

    got the hide/show on blogs/follows to work using jquery

--- public/js/microblog.js
+++ public/js/microblog.js
@@ -1,16 +1,34 @@
-// $(document).ready(function(){
-//   $("#logout").click(function(){
-//     $("#logout").hide();
-//     $("signin").show();
-//     $("signup").show();
-//   });
-//
-//   $("#signin").click(function(){
-//     $("#signin").hide();
-//     $("#signup").hide();
-//     $("#logout").show();
-//   });
-// });
+$(document).ready(function(){
+  $("#logout").click(function(){
+    $("#logout").hide();
+    $("signin").show();
+    $("signup").show();
+  });
+
+  $("#signin").click(function(){
+    $("#signin").hide();
+    $("#signup").hide();
+    $("#logout").show();
+  });
+
+  $(".blogbutton").click(function(){
+    $(".posts-follows").css("display", "flex");
+    $(".followers").css("display", "none");
+    $(".following").css("display", "none");
+  });
+
+  $(".followerbutton").click(function(){
+    $(".followers").css("display", "flex");
+    $(".posts-follows").css("display", "none");
+    $(".following").css("display", "none");
+  });
+
+  $(".followingbutton").click(function(){
+    $(".following").css("display", "flex");
+    $(".posts-follows").css("display", "none");
+    $(".followers").css("display", "none");
+  });
+});
 
 function confirmDelete() {
 if (window.confirm("Are you sure you want to delete your profile? There is no retreival")){
